90532703e5a98704471727ff809e5e5dcfc09014
ElPayo684 <eduardo_lg98@hotmail.com>
2025-09-30 15:21:21 -0600

    First project

--- TwoSum.py
+++ TwoSum.py
@@ -1,1 +1,13 @@
-print("hello world")
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+sol = Solution()
+nums = [2,7,11,15]
+target = 9
+result = sol.twoSum(nums, target)
+print(f"El resultado es: {result}")
